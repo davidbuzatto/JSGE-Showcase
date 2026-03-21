ad7b0e139bdae74b6866288d05c75a03510bad2c
David Buzatto <davidbuzatto@gmail.com>
2026-03-21 15:44:00 -0300

    2025 -> 2026

--- src/br/com/davidbuzatto/jsge/showcase/particles/ParticlesExample.java
+++ src/br/com/davidbuzatto/jsge/showcase/particles/ParticlesExample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Prof. Dr. David Buzatto
+ * Copyright (C) 2026 Prof. Dr. David Buzatto
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
